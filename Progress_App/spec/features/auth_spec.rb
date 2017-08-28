46a9af35b388ae161ff22e76004104a9ab51dc0a
App Academy Student <student@appacademy.io>
2017-08-28 14:47:52 -0700

    write code to pass auth_spec tests

--- Progress_App/spec/features/auth_spec.rb
+++ Progress_App/spec/features/auth_spec.rb
@@ -25,6 +25,8 @@ end
 
 feature 'logging in' do
 
+  let!(:user) { User.create!(username: "Johnny", password: "johnnyjohnny") }
+
   before(:each) do
     visit new_session_url
     fill_in 'Username', with: "Johnny"
@@ -39,8 +41,20 @@ feature 'logging in' do
 end
 
 feature 'logging out' do
-  scenario 'begins with a logged out state' do
-    expect(page).to have_content "Sign In"
+
+  let!(:user) { User.create!(username: "Johnny", password: "johnnyjohnny") }
+
+  before(:each) do
+    visit new_session_url
+    fill_in 'Username', with: "Johnny"
+    fill_in 'Password', with: "johnnyjohnny"
+    click_on "Sign In!"
+    visit goals_url
+    click_on "Sign Out"
+  end
+
+  scenario 'redirects to the new_session_url upon sign out' do
+    expect("http://www.example.com#{current_path}").to eq new_session_url
   end
 
   scenario 'doesn\'t show username on the homepage after logout' do
