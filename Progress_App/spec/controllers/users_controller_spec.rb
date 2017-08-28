a669727ced63e34c227357b7b1e9a06b43e7e7f3
App Academy Student <student@appacademy.io>
2017-08-28 13:47:40 -0700

    Pass all specs for user model and controller

--- Progress_App/spec/controllers/users_controller_spec.rb
+++ Progress_App/spec/controllers/users_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
   describe "POST #create" do
-    it "validates the presence of the user's email and password" do
+    it "validates the presence of the user's username and password" do
       post :create, params: { user: { username: "Jack", password: "" } }
       expect(response).to render_template("new")
       expect(flash[:errors]).to be_present
@@ -15,9 +15,9 @@ RSpec.describe UsersController, type: :controller do
     end
 
     context "with valid params" do
-      it "redirects user to sign-in page on success" do
+      it "redirects user to goals index page on success" do
         post :create, params: { user: { username: "Jack", password: "password" } }
-        expect(response).to redirect_to(new_session_url)
+        expect(response).to redirect_to(goals_url)
       end
     end
   end
