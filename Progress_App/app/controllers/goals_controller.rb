57724069315af629aea796342de9f08f912db2fc
App Academy Student <student@appacademy.io>
2017-08-28 17:58:35 -0700

    Power outage, no work lost, commiting anyway lol

--- Progress_App/app/controllers/goals_controller.rb
+++ Progress_App/app/controllers/goals_controller.rb
@@ -1,4 +1,24 @@
 class GoalsController < ApplicationController
+  before_action :require_logged_in, only: [:create]
+
   def index
+    if logged_in?
+      @goals = current_user.goals
+      render :index
+    else
+      @goals = Goal.all_public_goals
+      render :index
+    end
   end
+
+  def create
+    @goal = Goal.new(goal_params)
+    if @goal.save
+      redirect_to goal_url(@goal)
+    else
+      flash.now[:errors] = @goal.errors.full_messages
+    end
+  end
+
+  def new
 end
